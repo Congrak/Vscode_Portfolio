ffb7685248fab3f4a5d53c37502738d6c9c8265f
Congrak <105905676+Congrak@users.noreply.github.com>
2023-08-07 21:58:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vscode_Portfolio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-dcpgc4)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <p>I want look your code :)</p>
+    </div>
+  );
+}
